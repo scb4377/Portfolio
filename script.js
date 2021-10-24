d4229d90cabdd8d91c4152f2b5bfed2e74014114
Stephen Blevins <scblevins27@gmail.com>
2021-10-23 21:08:10 -0400

    Added comments

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const mvSource = document.querySelector('.music-visualiser-source');
 const contactForm = document.querySelector('.contact-form');
 const vh = window.innerHeight * 0.01;
 
+//variable for window height for mobile including toolbar
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 let headerHeight = header.offsetHeight;
@@ -25,17 +26,21 @@ window.addEventListener('scroll', () => {
     let titleSpeed = title.dataset.speed;
     let shadowSpeed = shadow.dataset.speed;
 
+    //title fade and lower on scroll
     title.style.transform = `translateY(${scroll * titleSpeed}px)`;
     title.style.opacity = - scroll / (headerHeight / 2) + 1;
 
-
+    //shadow increase height on scroll
     shadow.style.height = `${scroll * shadowSpeed + 100}px`;
 
 })
 
 hamburger.addEventListener('click', () => {
+
+    //toggle active for hamburger animation
     menu.classList.toggle('active')
 
+    //create animation ease in of links
     navLinks.forEach((link, index) => {
         if (link.style.animation) {
             link.style.animation = '';
@@ -45,6 +50,7 @@ hamburger.addEventListener('click', () => {
         }
     })
 
+    //hamburger animation
     hamLines.forEach(() => {
         if (menu.classList.contains('active')) {
             hamLines[0].style.opacity = '0';
@@ -58,33 +64,39 @@ hamburger.addEventListener('click', () => {
     })
 })
 
+//github link
 github.addEventListener('click', () => {
     window.open('https://github.com/scb4377', '_blank');
 })
 
+//linkedin link
 linkedIn.addEventListener('click', () => {
     window.open('https://www.linkedin.com/in/sblevinskw', '_blank');
 })
 
+//musicVisualizer demo link
 mvDemo.addEventListener('click', () => {
     window.open('https://scb4377.github.io/MusicVisualizer/', '_blank');
 })
 
+//musicVisualizer github source link
 mvSource.addEventListener('click', () => {
     window.open('https://github.com/scb4377/MusicVisualizer', '_blank');
 })
 
+//Add submitform listener
 window.addEventListener('submit', submitForm);
 
 //Submit form
-
 function submitForm() {
 
+    //Grab form values
     let fname = document.querySelector('#fname').value;
     let lname = document.querySelector('#lname').value;
     let email = document.querySelector('#email').value;
     let message = document.querySelector('#message').value;
 
+    //create object to pass emailJS
     let messageObject = {
         name: fname + " " + lname,
         email: email,
@@ -92,10 +104,14 @@ function submitForm() {
     }
 
     sendEmail(messageObject);
+
+    //Clear form on submit
     contactForm.reset();
 }
 
 function sendEmail(templateParams) {
+
+    //emailJS
     emailjs.send('service_kxar6ow', 'template_m2vhmlq', templateParams)
     .then(function(response) {
        console.log('SUCCESS!', response.status, response.text);
