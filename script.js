3753ffdc4622453a224c199a426b89de5051dd0e
Stephen Blevins <scblevins27@gmail.com>
2022-05-14 17:47:25 -0400

    pause videos when out of view

--- script.js
+++ script.js
@@ -1,276 +1,282 @@
-const title = document.querySelector('.title');
-const translate = document.querySelector('.translate');
-const header = document.querySelector('header');
-const shadow = document.querySelector('.shadow');
-const shadow2 = document.querySelector('.shadow2');
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.menu');
-const navLinks = document.querySelectorAll('.links');
-const hamLines = document.querySelectorAll('.line');
-const projects = document.querySelectorAll('.projectContainer');
-const projectPage = document.querySelector('#projects');
-const github = document.querySelector('.fa-github');
-const linkedIn = document.querySelector('.fa-linkedin');
-const mvDemo = document.querySelector('.music-visualiser-demo');
-const mvSource = document.querySelector('.music-visualiser-source');
-const contactForm = document.querySelector('.contact-form');
-const reDemo = document.querySelector('.real-estate-demo');
-const reSource = document.querySelector('.real-estate-source');
-const lcSource = document.querySelector('.loan-calculator-source');
-const lcDemo = document.querySelector('.loan-calculator-demo');
-const ctSource = document.querySelector('.crypto-tracker-source');
-const ctDemo = document.querySelector('.crypto-tracker-demo');
-const marvelDemo = document.querySelector('.marvel-demo');
-const marvelSource = document.querySelector('.marvel-source');
-const snowDemo = document.querySelector('.snowboard-demo');
-const snowSource = document.querySelector('.snowboard-source')
-const bugSource = document.querySelector('.bug-source')
-const bugDemo = document.querySelector('.bug-demo')
+const title = document.querySelector(".title");
+const translate = document.querySelector(".translate");
+const header = document.querySelector("header");
+const shadow = document.querySelector(".shadow");
+const shadow2 = document.querySelector(".shadow2");
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+const navLinks = document.querySelectorAll(".links");
+const hamLines = document.querySelectorAll(".line");
+const projects = document.querySelectorAll(".projectContainer");
+const projectPage = document.querySelector("#projects");
+const github = document.querySelector(".fa-github");
+const linkedIn = document.querySelector(".fa-linkedin");
+const mvDemo = document.querySelector(".music-visualiser-demo");
+const mvSource = document.querySelector(".music-visualiser-source");
+const contactForm = document.querySelector(".contact-form");
+const reDemo = document.querySelector(".real-estate-demo");
+const reSource = document.querySelector(".real-estate-source");
+const lcSource = document.querySelector(".loan-calculator-source");
+const lcDemo = document.querySelector(".loan-calculator-demo");
+const ctSource = document.querySelector(".crypto-tracker-source");
+const ctDemo = document.querySelector(".crypto-tracker-demo");
+const marvelDemo = document.querySelector(".marvel-demo");
+const marvelSource = document.querySelector(".marvel-source");
+const snowDemo = document.querySelector(".snowboard-demo");
+const snowSource = document.querySelector(".snowboard-source");
+const bugSource = document.querySelector(".bug-source");
+const bugDemo = document.querySelector(".bug-demo");
 const vh = window.innerHeight * 0.01;
-const cta = document.querySelector('.contact-me');
-const videoBg = document.getElementById('abstract');
-const musicVideo = document.querySelector('.musicVideo');
+const cta = document.querySelector(".contact-me");
+const videoBg = document.getElementById("abstract");
+const musicVideo = document.querySelector(".musicVideo");
+const mVid = document.querySelector(".mVid");
+const height = window.innerHeight;
+const width = window.innerWidth;
 
-window.addEventListener('load', () => {
-    videoBg.play()
-})
-// pause video
-// function myVideo() {
-//     videoBg.playbackRate = .9;
+// Set boundries of music visualizer video
+var bounding = musicVideo.getBoundingClientRect();
 
-//     setTimeout(() => {
-//         videoBg.pause();
-//     }, 2000)
-// }
-// setTimeout(myVideo, 7000)
+window.addEventListener("load", () => {
+  videoBg.play();
+});
 
 //variable for window height for mobile including toolbar
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty("--vh", `${vh}px`);
 
 let headerHeight = header.offsetHeight;
 
-window.addEventListener('scroll', () => {
-    let scroll = window.pageYOffset;
-    let titleSpeed = title.dataset.speed;
-    // let ctaSpeed = cta.dataset.speed;
-    let shadowSpeed = shadow.dataset.speed;
+window.addEventListener("scroll", () => {
+  let scroll = window.pageYOffset;
+  let titleSpeed = title.dataset.speed;
+  // let ctaSpeed = cta.dataset.speed;
+  let shadowSpeed = shadow.dataset.speed;
 
-    //title fade and lower on scroll
-    title.style.transform = `translateY(${scroll * titleSpeed}px)`;
-    title.style.opacity = - scroll / (headerHeight / 2) + 1;
+  //title fade and lower on scroll
+  title.style.transform = `translateY(${scroll * titleSpeed}px)`;
+  title.style.opacity = -scroll / (headerHeight / 2) + 1;
 
-    //cta on scroll
-    // cta.style.transform = `translateY(${scroll * titleSpeed}px)`;
-    // cta.style.opacity = - scroll / (headerHeight / 2) + 1;
+  //cta on scroll
+  // cta.style.transform = `translateY(${scroll * titleSpeed}px)`;
+  // cta.style.opacity = - scroll / (headerHeight / 2) + 1;
 
-    //shadow increase height on scroll
-    shadow.style.height = `${scroll * shadowSpeed + 100}px`;
-    let height = window.innerHeight
+  //shadow increase height on scroll
+  shadow.style.height = `${scroll * shadowSpeed + 100}px`;
 
-    if (height < scroll) {
-        videoBg.pause()
+  if (height < scroll && !videoBg.paused) {
+    videoBg.pause();
+  } else if (height > scroll && videoBg.paused) {
+    videoBg.play();
+  }
+
+  if (
+    bounding.bottom >= scroll &&
+    bounding.top - scroll <= height &&
+    musicVideo.paused
+  ) {
+    musicVideo.play();
+  } else if (
+    (bounding.bottom <= scroll &&
+      bounding.bottom - scroll <= height &&
+      !musicVideo.paused) ||
+    (bounding.top >= scroll + height && !musicVideo.paused)
+  ) {
+    musicVideo.pause();
+  }
+});
+
+hamburger.addEventListener("click", () => {
+  //toggle active for hamburger animation
+  menu.classList.toggle("active");
+
+  //create animation ease in of links
+  navLinks.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
     } else {
-        videoBg.play()
+      link.style.animation = `navLinkFade 0.5s ease forwards ${
+        index / 7 + 0.5
+      }s`;
     }
+  });
 
-})
-
-hamburger.addEventListener('click', () => {
-
-    //toggle active for hamburger animation
-    menu.classList.toggle('active')
-
-    //create animation ease in of links
-    navLinks.forEach((link, index) => {
-        if (link.style.animation) {
-            link.style.animation = '';
-        } else {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .5}s`;
-    
-        }
-    })
-
-    //hamburger animation
-    hamLines.forEach(() => {
-        if (menu.classList.contains('active')) {
-            hamLines[0].style.opacity = '0';
-            hamLines[1].style.transform = 'rotate(405deg)';
-            hamLines[2].style.transform = 'rotate(-405deg) translateY(-4px) translateX(4px)'
-        } else {
-            hamLines[0].style.opacity = '1';
-            hamLines[1].style.transform = '';
-            hamLines[2].style.transform = '';
-        }
-    })
-})
-
+  //hamburger animation
+  hamLines.forEach(() => {
+    if (menu.classList.contains("active")) {
+      hamLines[0].style.opacity = "0";
+      hamLines[1].style.transform = "rotate(405deg)";
+      hamLines[2].style.transform =
+        "rotate(-405deg) translateY(-4px) translateX(4px)";
+    } else {
+      hamLines[0].style.opacity = "1";
+      hamLines[1].style.transform = "";
+      hamLines[2].style.transform = "";
+    }
+  });
+});
 
 //github link
-github.addEventListener('click', () => {
-    window.open('https://github.com/scb4377', '_blank');
-})
+github.addEventListener("click", () => {
+  window.open("https://github.com/scb4377", "_blank");
+});
 
 //linkedin link
-linkedIn.addEventListener('click', () => {
-    window.open('https://www.linkedin.com/in/sblevinskw', '_blank');
-})
+linkedIn.addEventListener("click", () => {
+  window.open("https://www.linkedin.com/in/sblevinskw", "_blank");
+});
 
 //bug tracker demo link
-bugDemo.addEventListener('click', () => {
-    window.open('https://bug-tracker-react-node.herokuapp.com/', '_blank');
-})
+bugDemo.addEventListener("click", () => {
+  window.open("https://bug-tracker-react-node.herokuapp.com/", "_blank");
+});
 
 //bug tracker github source link
-bugSource.addEventListener('click', () => {
-    window.open('https://github.com/scb4377/bugTracker/tree/master', '_blank');
-})
+bugSource.addEventListener("click", () => {
+  window.open("https://github.com/scb4377/bugTracker/tree/master", "_blank");
+});
 
 //musicVisualizer demo link
-mvDemo.addEventListener('click', () => {
-    window.open('https://scb4377.github.io/MusicVisualizer/', '_blank');
-})
+mvDemo.addEventListener("click", () => {
+  window.open("https://scb4377.github.io/MusicVisualizer/", "_blank");
+});
 
 //musicVisualizer github source link
-mvSource.addEventListener('click', () => {
-    window.open('https://github.com/scb4377/MusicVisualizer', '_blank');
-})
+mvSource.addEventListener("click", () => {
+  window.open("https://github.com/scb4377/MusicVisualizer", "_blank");
+});
 
-snowDemo.addEventListener('click', () => {
-    window.open('https://snowboards-sk.herokuapp.com/', '_blank');
-})
+snowDemo.addEventListener("click", () => {
+  window.open("https://snowboards-sk.herokuapp.com/", "_blank");
+});
 
-snowSource.addEventListener('click', () => {
-    window.open('https://github.com/scb4377/snowboard-app/tree/master', '_blank');
-})
+snowSource.addEventListener("click", () => {
+  window.open("https://github.com/scb4377/snowboard-app/tree/master", "_blank");
+});
 
 //marvel demo link
-marvelDemo.addEventListener('click', () => {
-    window.open('https://scb4377.github.io/marvel/', '_blank');
-})
+marvelDemo.addEventListener("click", () => {
+  window.open("https://scb4377.github.io/marvel/", "_blank");
+});
 
 //marvel github source link
-marvelSource.addEventListener('click', () => {
-    window.open('https://github.com/scb4377/marvel/tree/master/client', '_blank');
-})
+marvelSource.addEventListener("click", () => {
+  window.open("https://github.com/scb4377/marvel/tree/master/client", "_blank");
+});
 
 //cryptoTracker github source link
-ctSource.addEventListener('click', () => {
-    window.open('https://github.com/scb4377/CryptoTracker/tree/gh-pages', '_blank');
-})
+ctSource.addEventListener("click", () => {
+  window.open(
+    "https://github.com/scb4377/CryptoTracker/tree/gh-pages",
+    "_blank"
+  );
+});
 
 //cryptoTracker demo link
-ctDemo.addEventListener('click', () => {
-    window.open('https://scb4377.github.io/CryptoTracker/', '_blank');
-})
+ctDemo.addEventListener("click", () => {
+  window.open("https://scb4377.github.io/CryptoTracker/", "_blank");
+});
 
 //Real estate mockup demo link
-reDemo.addEventListener('click', () => {
-    window.open('https://scb4377.github.io/Real-Estate-Mockup/', '_blank');
-})
+reDemo.addEventListener("click", () => {
+  window.open("https://scb4377.github.io/Real-Estate-Mockup/", "_blank");
+});
 
 //Real estate mockup source link
-reSource.addEventListener('click', () => {
-    window.open('https://github.com/scb4377/Real-Estate-Mockup/tree/master', '_blank');
-})
+reSource.addEventListener("click", () => {
+  window.open(
+    "https://github.com/scb4377/Real-Estate-Mockup/tree/master",
+    "_blank"
+  );
+});
 
-lcDemo.addEventListener('click', () => {
-    window.open('https://scb4377.github.io/Loan-Calculator/', '_blank');
-})
+lcDemo.addEventListener("click", () => {
+  window.open("https://scb4377.github.io/Loan-Calculator/", "_blank");
+});
 
-lcSource.addEventListener('click', () => {
-    window.open('https://github.com/scb4377/Loan-Calculator', '_blank');
-})
+lcSource.addEventListener("click", () => {
+  window.open("https://github.com/scb4377/Loan-Calculator", "_blank");
+});
 
 //Add submitform listener
-window.addEventListener('submit', submitForm);
+window.addEventListener("submit", submitForm);
 
 // Verify form
 function formValidation(box, index) {
-    let classes = (classes) => document.getElementsByClassName(classes);
-    let errMsg = classes('error');
-    let testBool = false;
+  let classes = (classes) => document.getElementsByClassName(classes);
+  let errMsg = classes("error");
+  let testBool = false;
 
-    if (box == "") {
-        errMsg[index].textContent = "* Sorry, this cannot be left blank";
-    }
-    else {
-        errMsg[index].textContent = "";
-        testBool = true
-    }
-    return testBool;
-
+  if (box == "") {
+    errMsg[index].textContent = "* Sorry, this cannot be left blank";
+  } else {
+    errMsg[index].textContent = "";
+    testBool = true;
+  }
+  return testBool;
 }
 
 function verifyEmail(email, index) {
-    let verifyEmail = /^\w+[\+\.\w-]*@([\w-]+\.)*\w+[\w-]*\.([a-z]{2,4}|\d+)$/i;
-    let emailErrMsg = document.querySelector('.verify-email');
+  let verifyEmail = /^\w+[\+\.\w-]*@([\w-]+\.)*\w+[\w-]*\.([a-z]{2,4}|\d+)$/i;
+  let emailErrMsg = document.querySelector(".verify-email");
 
-    if (!verifyEmail.test(email) || email == "") {
-        emailErrMsg.textContent = "* Sorry, please enter a valid email";
-        return false;
-    }
-    else {
-        emailErrMsg.textContent = "";
-        return true;
-    }
+  if (!verifyEmail.test(email) || email == "") {
+    emailErrMsg.textContent = "* Sorry, please enter a valid email";
+    return false;
+  } else {
+    emailErrMsg.textContent = "";
+    return true;
+  }
 }
 
 //Submit form
 function submitForm(e) {
-    e.preventDefault()
-    
+  e.preventDefault();
 
-    //Grab form values
-    let fname = document.querySelector('#fname').value;
-    let lname = document.querySelector('#lname').value;
-    let email = document.querySelector('#email').value;
-    let message = document.querySelector('#message').value;
+  //Grab form values
+  let fname = document.querySelector("#fname").value;
+  let lname = document.querySelector("#lname").value;
+  let email = document.querySelector("#email").value;
+  let message = document.querySelector("#message").value;
 
-    let check1 = formValidation(fname, 0);
-    let check2 = formValidation(lname, 1);
-    let check3 = verifyEmail(email, 2)
-    let check4 = formValidation(message, 3);
+  let check1 = formValidation(fname, 0);
+  let check2 = formValidation(lname, 1);
+  let check3 = verifyEmail(email, 2);
+  let check4 = formValidation(message, 3);
 
-    function check() {
-        if (check1 && check2 && check3 && check4) {
-            return true;
-        }
-        else {
-            return false;
-        }
+  function check() {
+    if (check1 && check2 && check3 && check4) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-
-    //Validate inputs
-    if (check()) {
-
+  //Validate inputs
+  if (check()) {
     // create object to pass emailJS
-        let messageObject = {
-            name: fname + " " + lname,
-            email: email,
-            message: message
-        }
-
-        //send email if verified
-        sendEmail(messageObject);
-        document.querySelector('.error').textContent = "";
+    let messageObject = {
+      name: fname + " " + lname,
+      email: email,
+      message: message,
     };
 
-    //Clear form on submit
-    contactForm.reset();
+    //send email if verified
+    sendEmail(messageObject);
+    document.querySelector(".error").textContent = "";
+  }
+
+  //Clear form on submit
+  contactForm.reset();
 }
 
 function sendEmail(templateParams) {
-
-    //emailJS
-    emailjs.send('service_kxar6ow', 'template_m2vhmlq', templateParams)
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
+  //emailJS
+  emailjs.send("service_kxar6ow", "template_m2vhmlq", templateParams).then(
+    function (response) {
+      console.log("SUCCESS!", response.status, response.text);
+    },
+    function (error) {
+      console.log("FAILED...", error);
+    }
+  );
 }
-
-
-
-
