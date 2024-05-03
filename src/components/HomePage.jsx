11689d47d8a6c128934dfbdc8d7314aaeffabffe
Stephen Blevins <61333980+sblevins-dev@users.noreply.github.com>
2024-05-03 16:00:49 -0400

    updated the about me

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -6,7 +6,7 @@ import { MyContext } from './MyContext';
 const HomePage = () => {
 
     const { sectionRefs, scrollTo } = useContext(MyContext);
-    
+
     return (
         <Container
             ref={sectionRefs[0]}
@@ -16,7 +16,7 @@ const HomePage = () => {
                 alignItems: 'center',
                 flexWrap: 'wrap',
                 gap: 2,
-                justifyContent: { sm: 'center', lg: 'space-between'},
+                justifyContent: { sm: 'center', lg: 'space-between' },
                 minHeight: '100vh',
             }}
         >
@@ -33,7 +33,7 @@ const HomePage = () => {
                     SOFTWARE ENGINEER AND WEB DEVELOPER
                 </Typography>
                 <Button variant='contained'
-                onClick={() => scrollTo(2)}
+                    onClick={() => scrollTo(2)}
                     sx={{
                         marginY: 2,
                         bgcolor: '#272727',
@@ -53,7 +53,7 @@ const HomePage = () => {
                     borderRadius: '2px',
                     padding: '30px',
                     backdropFilter: "blur(5px) saturate(100%)",
-                    display: { xs: 'none', sm: 'flex'},
+                    display: { xs: 'none', sm: 'flex' },
                     flexDirection: 'column'
                 }}>
                 <Typography variant='h6'
@@ -65,13 +65,7 @@ const HomePage = () => {
                     ABOUT
                 </Typography>
                 <Typography variant='body1'>
-                    &emsp; With over a decade in customer service and retail management,
-                    coupled with a Bachelor of Science in Computer Information Systems specializing in software programming,
-                    I bring a robust skill set to software development. Proficient in MERN stack, Java technologies,
-                    and versed in various tools, I excel in understanding end-user perspectives and
-                    translating them into user-friendly software solutions.
-                    Eager for a role where I can further expand my knowledge and skills while contributing
-                    effectively to innovative projects.
+                    &emsp; As a dedicated professional with over a decade of experience in customer service and retail management, complemented by a Bachelor of Science in Computer Information Systems specializing in software programming, I offer a strong foundation for software development. Proficient in the MERN stack, Java technologies, and adept in various development tools, I excel in crafting user-friendly software solutions by leveraging my understanding of end-user perspectives. I am eager to transition into a junior software engineering role to further expand my knowledge and skills while actively contributing to innovative projects.
                 </Typography>
 
             </Box>
