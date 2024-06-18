a49b7d2cb635a141e634ce264e0b76c335392c9c
Stephen Blevins <61333980+sblevins-dev@users.noreply.github.com>
2024-06-18 13:35:39 -0400

    First commit of v4

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import Hero from "@/components/Hero";
+import Image from "next/image";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
+import Grid from "@/components/Grid";
+
+export default function Home() {
+  return (
+    <main
+      className="relative bg-black-100 flex 
+      justify-center items-center flex-col overflow-hidden 
+      mx-auto sm:px-10 px-5"
+    >
+      <div
+        className="max-w-7xl w-full"
+      >
+        <h1>Hello, Portfolio</h1>
+        <FloatingNav
+          navItems={[{name: 'Home', link: '/', icon: <FaHome />}]}
+        />
+        <Hero />
+        <Grid />
+      </div>
+    </main>
+  );
+}
