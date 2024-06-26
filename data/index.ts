6cb810d02e6ad60df76b2de34ffcd0e07ed001ab
Stephen Blevins <61333980+sblevins-dev@users.noreply.github.com>
2024-06-26 11:46:52 -0400

    commit

--- data/index.ts
+++ data/index.ts
@@ -6,21 +6,21 @@ export const navItems = [
   ];
   
   export const gridItems = [
-    {
-      id: 1,
-      title: "I prioritize client collaboration, fostering open communication ",
-      description: "",
-      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
-      imgClassName: "w-full h-full",
-      titleClassName: "justify-end",
-      img: "/b1.svg",
-      spareImg: "",
-    },
+    // {
+    //   id: 1,
+    //   title: "I prioritize client collaboration, fostering open communication ",
+    //   description: "",
+    //   className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    //   imgClassName: "w-full h-full",
+    //   titleClassName: "justify-end",
+    //   img: "/b1.svg",
+    //   spareImg: "",
+    // },
     {
       id: 2,
       title: "I'm very flexible with time zone communications",
       description: "",
-      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4",
       imgClassName: "",
       titleClassName: "justify-start",
       img: "",
@@ -61,7 +61,7 @@ export const navItems = [
       id: 6,
       title: "Do you want to start a project together?",
       description: "",
-      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
       imgClassName: "",
       titleClassName: "justify-center md:max-w-full max-w-60 text-center",
       img: "",
